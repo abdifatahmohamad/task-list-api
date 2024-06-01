a015f949e2a1837b485968fb68948fb1ece9a831
Abdifatah Mohamed <mofaatah@gmail.com>
2024-06-01 04:42:37 -0500

    Fixed cors error

--- src/main/java/com/example/task_list_api/config/CorsConfig.java
+++ src/main/java/com/example/task_list_api/config/CorsConfig.java
@@ -2,10 +2,10 @@ package com.example.task_list_api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
-@CrossOrigin
+//@CrossOrigin
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
