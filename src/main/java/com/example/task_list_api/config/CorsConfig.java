5902a9dad9cc5bb93b5263220e73c6e72e05ad30
Abdifatah Mohamed <mofaatah@gmail.com>
2024-06-01 04:47:52 -0500

    Fixed cors error

--- src/main/java/com/example/task_list_api/config/CorsConfig.java
+++ src/main/java/com/example/task_list_api/config/CorsConfig.java
@@ -2,17 +2,16 @@ package com.example.task_list_api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
-//@CrossOrigin
+@CrossOrigin
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:3000", "https://merry-souffle-8b1c2d.netlify.app")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://merry-souffle-8b1c2d.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
